a2e62d5238b6778a3d373b1dc8a2f79016fc46e8
Bonujumanova <bonujumanova@gmail.com>
2026-06-26 17:09:45 +0700

    Добавлено решение заданий

--- tests/test_markers.py
+++ tests/test_markers.py
@@ -29,11 +29,9 @@ class TestUserAuthentification:
 
     def test_logout(self):
         ...
-    
+
 @pytest.mark.smoke
 @pytest.mark.regression
 @pytest.mark.critical
 def test_critical_login():
     ...
-
-
